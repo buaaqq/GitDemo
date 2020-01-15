3d4a3910ae42cb8f01f65e621549fd7a8818a13e
buaaqq <xkou1101@gmail.com>
2020-01-15 16:14:15 -0500

    Add two_sum code to the file

--- file1.py
+++ file1.py
@@ -1,3 +1,16 @@
 # git demo
 # add code to the file
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 12, 5]
+    target = 9
+    answer = Solution().twoSum(nums, target)
+    print(answer)
