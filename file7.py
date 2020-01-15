bcb968676f1101d426071939ede704a086d2965f
buaaqq <xkou1101@gmail.com>
2020-01-15 16:23:10 -0500

    Add file7

---
+++ file7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x):
+        y = 0
+        if x >= 0:
+            flag = 1
+            y = int(str(x)[::-1]) * flag
+        else:
+            flag = -1
+            y = int(str(abs(x))[::-1]) * flag
+
+        if y > 2 ** 31 - 1 or y < -2 ** 31:
+            return 0
+        else:
+            return y
+
+if __name__ =="__main__":
+    x = 123
+    y = Solution().reverse(x)
+    print(y)
